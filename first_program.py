60970e9f02b16564e99e639ea9ae909594be4f20
JIGNESHGRANDHE <randhejigneshgit@gmail.com>
2025-09-04 22:12:49 +0530

    python/first_program.py
    This script covers:
    - Printing text in Python
    - Adding new lines using \n and triple quotes
    - Single-line and multi-line comments
    - Handling apostrophes inside strings using escape characters

---
+++ first_program.py
@@ -0,0 +1,17 @@
+#first program in python
+print("Hello World")
+#how to add new line method one
+print("pune\nis best")
+print("""Indian is Great 
+country.""")
+#how to add comment in single line
+"""how to add comment for multiple lines"""
+
+#why do we use only double quotation or triple quotation in python
+#('it's a good thing')
+
+#above example will make a syntax error
+#for that we used double or triple quotaion mostly
+
+#solution
+print('it\'s a good thing')
