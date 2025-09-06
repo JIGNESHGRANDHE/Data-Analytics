b5b28a7d4e563611c9878fe3fd1f91c20b767828
JIGNESHGRANDHE <randhejigneshgit@gmail.com>
2025-09-06 20:17:14 +0530

    first_program.py

--- python/first_program.py
+++ python/first_program.py
@@ -1,9 +1,18 @@
 #first program in python
 print("Hello World")
+#output=Hello World
 #how to add new line method one
 print("pune\nis best")
-print("""Indian is Great 
+#output 
+"""pune
+  is best"""
+
+
+print("""India is Great 
 country.""")
+#output
+"""India is great
+   country."""
 #how to add comment in single line
 """how to add comment for multiple lines"""
 
@@ -15,3 +24,5 @@ country.""")
 
 #solution
 print('it\'s a good thing')
+#output
+# it's a good thing
