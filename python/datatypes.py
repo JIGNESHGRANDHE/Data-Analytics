a0e1f1290c1fb32f0eea9d90ff710c7416cb3a0f
JIGNESHGRANDHE <randhejigneshgit@gmail.com>
2025-09-08 20:02:04 +0530

    Add simple examples of String, Integer, and Float datatypes
    - Demonstrated string datatype with variables (name, city) and output
    - Demonstrated integer datatype with whole number input (birth_year, students count)
    - Demonstrated float datatype with decimal input (height, weight)
    - Added inline comments and example outputs for clarity
    - Beginner-friendly syntax showing where each datatype is used

---
+++ python/datatypes.py
@@ -0,0 +1,31 @@
+# String
+
+name = "Jignesh"      # syntax= text inside quotes only for string datatype
+city = "Pune"
+print("Name:", name)
+print("City:", city)
+
+""" string output
+Name:Jignesh
+City:Pune
+"""
+
+# Integer
+# only use for whole numbers
+birth_year = 2004
+print("Birth_year=", birth_year)
+int(input("enter the students in 10 class="))
+"""integer output
+birth_year 2004
+enter the students in 10 class=45
+"""
+
+
+# Float
+# use for specificaly for decimal number
+height = 5.9
+print("Height", height)
+float(input("Enter the weight="))
+"""float ouput
+Height 5.9
+Enter the weight=45.26"""
