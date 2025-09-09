27a9811ce99db46b8702bbd124a3c8d86c630bdc
JIGNESHGRANDHE <randhejigneshgit@gmail.com>
2025-09-09 18:37:36 +0530

    Python programs for arithmetic operations
    This commit includes examples of basic arithmetic operations in Python:
    - Addition of numbers
    - Subtraction of numbers
    - Multiplication of numbers
    - Division (returns float)
    - Floor Division (integer result)
    - Modulus (remainder)
    - Exponentiation (power calculation)
    
    Each section demonstrates input, calculation, and expected output.

---
+++ python/arithmetic_operator.py
@@ -0,0 +1,108 @@
+print("\nAddition")
+q1=int(input("enter first number="))
+q2=int(input("enter second number="))
+q3=(q1+q2)
+print("\naddition of two numbers=",q3)
+"""
+#OUTPUT
+Addition
+enter first number=14
+enter second number=14
+
+addition of two numbers= 28
+"""
+
+
+print("\nSubtraction")
+w1=int(input("enter first number="))
+w2=int(input("enter second number="))
+w3=(w1-w2)
+print("\nsubtraction of two numbers=",w3)
+"""
+#OUTPUT
+Subtraction
+enter first number=25
+enter second number=26
+
+subtraction of two numbers= -1
+"""
+
+
+print("\nMultiplication")
+e1=int(input("enter first number="))
+e2=int(input("enter second number="))
+e4=int(input("enter third number="))
+e3=(e1*e2*e4)
+print("\nmultiplication of two numbers=",e3)
+"""
+#OUTPUT
+Multiplication
+enter first number=30
+enter second number=1
+enter third number=56
+
+multiplication of two numbers= 1680
+"""
+
+
+print("\ndivision")
+r1=int(input("enter first number="))
+r2=int(input("enter second number="))
+r3=(r1/r2)
+print("\ndivision of two numbers=",r3)
+
+"""
+#OUTPUT
+division
+enter first number=98
+enter second number=49
+
+division of two numbers= 2.0
+"""
+
+
+print("\nFloor division")
+t1=int(input("enter first number="))
+t2=int(input("enter second number="))
+t3=(t1//t2)
+print("\nFloor division of two numbers=",t3)
+"""
+#OUTPUT
+Floor division
+enter first number=23
+enter second number=11
+
+Floor division of two numbers= 2
+"""
+
+
+print("\nModulus")
+y1=int(input("enter first number="))
+y2=int(input("enter second number="))
+y3=(y1%y2)
+print("\nmodulus of two numbers=",y3)
+"""
+#OUTPUT
+Modulus
+enter first number=25
+enter second number=3
+
+modulus of two numbers= 1
+"""
+
+
+print("\nExponentiation")
+u1=int(input("enter first number="))
+u2=int(input("enter second number="))
+u3=(u1**u2)
+print("\nExponentiation of two numbers=",u3)
+"""
+#OUTPUT
+Exponentiation
+enter first number=5
+enter second number=3
+
+Exponentiation of two numbers= 125
+"""
+
+
