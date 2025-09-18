eddf91dcfe32709c5f5c17e7572230c70b6acd1a
JIGNESHGRANDHE <randhejigneshgit@gmail.com>
2025-09-18 22:18:35 +0530

    A simple Python demonstration of nested for loops to generate two common number patterns.
    This code showcases the fundamental programming concept of nested loops by creating two distinct numerical patterns. It's an excellent example for beginners like me to understand loop control variables and how inner loop ranges can be dynamically set by the outer loop.

---
+++ python/python_loops/nested_loops.py
@@ -0,0 +1,32 @@
+
+for i in range(0,6):
+    for j in range(i+1,6):
+            print(j,end="",)
+    print()
+""" 
+#OUTPUT
+12345
+2345
+345
+45
+5
+
+"""
+
+
+
+for i in range(1,6):
+    for j in range(1,i+1):
+            print(j,end="")
+    print()
+
+
+"""
+#OUTPUT
+1
+12
+123
+1234
+12345
+"""
+
