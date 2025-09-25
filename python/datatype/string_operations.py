7b369689ddf3fbb1d7003a0723639f2aeeaf837b
JIGNESHGRANDHE <randhejigneshgit@gmail.com>
2025-09-25 22:47:16 +0530

    Update string_operations.py
    - Included actual program outputs for each string method
    
    - Used triple-quote docstrings to display expected results

--- python/datatype/string_operations.py
+++ python/datatype/string_operations.py
@@ -2,15 +2,34 @@
 a ="HUMAN"
 b="{} shared 90% DNA with monkeys & gorillas."
 print(b.format(a))
+"""
+#OUTPUT
+HUMAN shared 90% DNA with monkeys & gorillas.
+"""
 
 # capitalize():- makes the first letter uppercase and the rest lowercase
 c="Lion iS from cAt family "
 print(c.capitalize())
+"""
+#OUTPUT
+Lion is from cat family 
+"""
 
 # casefold():- converts the entire string into lowercase
 d="First BIG house in Town"
 print(d.casefold())
+"""
+#OUTPUT
+first big house in town
+"""
 
 # center():- Adds characters on both sides to make text appear in middle
 print(a.center(20))
+
 print(a.center(13,"|"))
+"""
+#OUTPUT
+       HUMAN        
+||||HUMAN||||
+
+"""
