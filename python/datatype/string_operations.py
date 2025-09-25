e57b5060910d93938ae87ec9c912e1d78d0e6764
JIGNESHGRANDHE <randhejigneshgit@gmail.com>
2025-09-25 22:37:53 +0530

    String Methods Practice: format, capitalize, casefold, center
    Demonstrated usage of common Python string methods.
    
    1) format() to insert values inside placeholders.
    
    2) capitalize() to convert the first character to uppercase.
    
    3) casefold() to fully lowercase a string.
    
    4) center() to align text in the middle with custom padding.

---
+++ python/datatype/string_operations.py
@@ -0,0 +1,16 @@
+# format():- inserts a value inside {} placeholders in a string
+a ="HUMAN"
+b="{} shared 90% DNA with monkeys & gorillas."
+print(b.format(a))
+
+# capitalize():- makes the first letter uppercase and the rest lowercase
+c="Lion iS from cAt family "
+print(c.capitalize())
+
+# casefold():- converts the entire string into lowercase
+d="First BIG house in Town"
+print(d.casefold())
+
+# center():- Adds characters on both sides to make text appear in middle
+print(a.center(20))
+print(a.center(13,"|"))
