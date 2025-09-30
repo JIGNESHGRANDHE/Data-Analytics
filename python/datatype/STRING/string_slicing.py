a7e84c3dcaaf41a5b16d0d614ee0a59669edd5d6
JIGNESHGRANDHE <randhejigneshgit@gmail.com>
2025-09-30 17:18:04 +0530

    Add string slicing examples in Python.
    Demonstrated different string slicing techniques.
    
    Covered:
    Extracting substrings with positive indices.
    Using negative indices for reverse access.
    Applying step values to skip characters.
    Reversing entire strings.
    Examples include practical strings ("Running is good for health") and numeric sequence ("0123456789").

---
+++ python/datatype/STRING/string_slicing.py
@@ -0,0 +1,14 @@
+
+
+a="Running is good for health"
+print(a[:])          #Running is good for health
+print(a[0:16])       #Running is good
+print(a[16:])        #for health
+print(a[::-1])       #htlaeh rof doog si gninnuR
+
+
+b="0123456789"
+print(b[0:2])       #01
+print(b[::3])       #0369
+print(b[1:7:2])     #135
+print(b[::-1])      #9876543210
