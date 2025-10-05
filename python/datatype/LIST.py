04fe2454e4347f439411c60d09ba4579795e9304
JIGNESHGRANDHE <randhejigneshgit@gmail.com>
2025-10-05 22:26:20 +0530

    Added a simple Python program demonstrating the creation list.
    - Defined a list named 'planets' containing mixed data types (floats ,integers and strings)
    - Printed the entire list to display its contents
    - Used the type() function to confirm the data structure as a list

---
+++ python/datatype/LIST.py
@@ -0,0 +1,10 @@
+planets=[3,"Earth",4,"Mars",50.2,"Saturn","Jupiter","Neptune"]
+print(planets)
+print(type(planets))
+
+"""
+#OUTPUT
+[3, 'Earth', 4, 'Mars', 50.2, 'Saturn', 'Jupiter', 'Neptune']
+<class 'list'>
+
+"""
