e17a092dd92011cf940dc177419e2c858cdf9be1
JIGNESHGRANDHE <randhejigneshgit@gmail.com>
2025-10-06 18:57:02 +0530

    Python List Operations – Creation, Modification, and Manipulation.
     1. Creates a list containing names of different animals.
     2. Prints the entire list.
     3. Finds and prints the total number of elements using len().
     4. Counts how many times a particular element appears using count().
     5. Adds a new element at the end of the list using append().
     6. Inserts an element at a specific position using insert().
     7. Removes a specific element by name using remove().
     8. Removes an element at a specific index using pop().
     9. Displays the updated list after each operation.

---
+++ python/datatype/LIST/List_functions.py
@@ -0,0 +1,31 @@
+a=["Dog","Cat","horse","Goat","cow","cat"]
+print(a)
+print("*"*50)
+
+#Write a program to find the length of list
+print("the length of sting is=",len(a))
+print("*"*50)
+
+#Write a program to count an oocurence of particular element
+print(a.count("Cat"))
+print(a.count("dog"))
+print("*"*50)
+
+#Write a program to add element in the list
+a.append("Rabbit")
+print(a)
+print("*"*50)
+
+#Write a program to add element at specific position
+a.insert(0,"Donkey")
+print(a)
+print("*"*50)
+
+#Write a program to remove an element from list
+a.remove("cat")
+print(a)
+print("*"*50)
+
+#Write a program to remove an element from certain position
+a.pop(2)
+print(a)
