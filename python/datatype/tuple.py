1e3324df461ae96fdbbc7e5b1bafff001823d2dd
Jignesh G Randhe <randhejigneshgit@gmail.com>
2025-10-11 22:42:48 +0530

    Tuple Creation and Identification in Python.
    This program demonstrates different ways to create and identify tuples in Python.
    It shows that tuples can be created :-
    With or without parentheses ().
    Even with a single element, but a comma ( , ) is required.
    Tuples can hold mixed data types like strings, integers, and floats.

---
+++ python/datatype/tuple.py
@@ -0,0 +1,18 @@
+g="Football","Basketball","Chess","Cricket","Swimming"       #tuple without parentheses
+g1=("Football","Basketball","Chess","Cricket","Swimming")    #tuple with parentheses
+
+print(type(g1))   #<class 'tuple'>
+print(type(g))    #<class 'tuple'>
+
+
+f="Engineering"
+print(type(f))   #<class 'str'>
+
+f="Engineering",
+f1=("Engineering",)
+print(type(f1)) #<class 'tuple'>
+print(type(f))  #<class 'tuple'>
+
+
+h=("cat", "dog", 9, 6, 85.56)
+print(type(h))  #<class 'tuple'>
