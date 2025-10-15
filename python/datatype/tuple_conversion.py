bd78a82506905058b13d397cdf6db979839f041d
Jignesh G Randhe <randhejigneshgit@gmail.com>
2025-10-15 22:26:50 +0530

    Converting Between Tuple and List in Python
    This program demonstrates how to:-
    Create a tuple containing multiple elements.
    Convert a tuple to a list using the list() function.
    Convert the list back to a tuple using the tuple() function.
    Verify the data type after each conversion using type().

---
+++ python/datatype/tuple_conversion.py
@@ -0,0 +1,29 @@
+a=("laptop","windows","computer","mouse")
+print(a)
+print(type(a))
+
+
+"""
+#OUTPUT
+('laptop', 'windows', 'computer', 'mouse')
+<class 'tuple'>
+"""
+
+b=list(a)
+print(b)
+print(type(b))
+
+"""
+#OUTPUT
+['laptop', 'windows', 'computer', 'mouse']
+<class 'list'>
+"""
+
+c=tuple(b)
+print(c)
+print(type(c))
+"""
+#OUTPUT
+('laptop', 'windows', 'computer', 'mouse')
+<class 'tuple'>
+"""
