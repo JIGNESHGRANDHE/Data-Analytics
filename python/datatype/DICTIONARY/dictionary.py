c0ece9f69ed0ad577c46db7915034d51785993b7
Jignesh G Randhe <randhejigneshgit@gmail.com>
2025-10-17 21:45:51 +0530

    Update dictionary.py

--- python/datatype/DICTIONARY/dictionary.py
+++ python/datatype/DICTIONARY/dictionary.py
@@ -1,3 +1,10 @@
 employee_data={'first_name': 'Hitesh','last_name': 'Varma','age': 22,'gender': 'male'}
 print(employee_data)
 print(type(employee_data))
+
+"""
+#OUTPUT:-
+{'first_name': 'Hitesh', 'last_name': 'Varma', 'age': 22, 'gender': 'male'}
+<class 'dict'>
+"""
+
