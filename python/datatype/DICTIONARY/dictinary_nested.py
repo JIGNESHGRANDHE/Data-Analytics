933a5d625c9feade92b4cb133cb0796145245de3
Jignesh G Randhe <randhejigneshgit@gmail.com>
2025-10-22 20:37:50 +0530

    Update dictinary_nested.py

--- python/datatype/DICTIONARY/dictinary_nested.py
+++ python/datatype/DICTIONARY/dictinary_nested.py
@@ -1,3 +1,4 @@
+
 Employees={1:{'name':'Jack','age':25,'emp_id':111,'Gender':'Male'},
            2:{'name':'Julia','age':29,'emp_id':112,'Gender':'Male'},
            3:{'name':'Olly','age':35,'emp_id':113,'Gender':'Female'}}
@@ -11,3 +12,4 @@ print(Employees[2]) #{'name': 'Julia', 'age': 29, 'emp_id': 112, 'Gender': 'Male
 print(Employees[3]) #{'name': 'Olly', 'age': 35, 'emp_id': 113, 'Gender': 'Female'}
 
 print(Employees[3]['name'],'(',Employees[3]['Gender'],')')  #Olly ( Female )
+
