746b0c148de2d52eea4852f0cfbb607444315334
Jignesh G Randhe <randhejigneshgit@gmail.com>
2025-10-28 22:21:09 +0530

    Iteration in Python Set.
    This program demonstrates how to create a set and iterate through its elements in Python.
    A set is an unordered collection of unique items, so the order of output may vary each time the program runs.

---
+++ python/datatype/SETS/sets_iteration.py
@@ -0,0 +1,20 @@
+a={"India","China","Nepal","Bhutan","Sri Lanka"}
+print(a)
+
+"""
+{'Nepal', 'Sri Lanka', 'India', 'Bhutan', 'China'}
+"""
+
+#Iteration in set
+for x in a:
+    print(x)
+
+"""
+#OUTPUT:-
+Nepal
+Sri Lanka
+India
+Bhutan
+China
+"""
+
